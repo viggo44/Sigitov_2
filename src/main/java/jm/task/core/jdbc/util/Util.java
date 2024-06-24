8e5ff04989e5eb38431e0b22a0adf5ba02c2a9d6
Artem Sigitov <art200119@gmail.com>
2024-06-24 21:29:07 +0300

    update Util, remove resultSet

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,18 +1,13 @@
 package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Util {
     private static final String url = "jdbc:mysql://localhost:3306/mydbtest";
     private static final String user = "root";
     private static final String password = "Belka130277/";
-
     private static Connection con;
-    private static Statement stmt;
-    private static ResultSet rs;
 
     public static Connection getConnection(){
         Connection con = null;
