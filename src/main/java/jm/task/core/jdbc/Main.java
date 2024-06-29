596cddb08b3c2a47f1b86ecb16819e22ad5c794d
Artem Sigitov <art200119@gmail.com>
2024-06-29 23:38:21 +0300

    new branch2

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,7 +11,6 @@ public class Main {
         userdao.createUsersTable();
 User user0 = new User("Artem0", "Sigitov", (byte) 23);
         User user1 = new User("Artem1", "Sigitov", (byte) 23);
-        User user2 = new User("Artem2", "Sigitov", (byte) 23);
         userdao.saveUser("A","S",(byte) 2);
         userdao.saveUser("b","S",(byte) 2);
         userdao.saveUser("c","S",(byte) 2);
